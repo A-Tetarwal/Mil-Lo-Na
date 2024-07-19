73dea6df03789069cacd914bf3bc68389fdf5697
A-Tetarwal <andashishwill@gmail.com>
2024-07-19 08:23:56 +0530

    Edit File

--- index.js
+++ index.js
@@ -37,16 +37,41 @@ app.post('/create', (req, res) => { // post me form se jo
 })
 
 app.get('/edit/:filename', (req, res) => { // idhar wala params yahan se parameter bhejta hai, taaki hm usko ejs mein use kr sakein
-    res.render('edit', { filename: req.params.filename });
-})
-
-app.post('/edit', (req, res) => { // udhar form submit hone pe humko new aur previous do cheezien milengi
-    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
-        if (err) console.log(err);
-        else console.log('done');
-        res.redirect('/')
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, filedata) => {
+        const filedataArray = filedata.split(','); // split hoke ek array me save ho jata hai 
+        res.render('edit', { filename: req.params.filename, filedata: filedataArray }); // filedata params me nahin aayega, kyunki iss wale app.get me to sirf filename hi hai parameter mein
     })
 })
+
+app.post('/edit', (req, res) => {
+    const previousFilename = req.body.previousName;
+    const newFilename = req.body.newName.trim() !== '' ? req.body.newName : previousFilename;
+    const fileContent = req.body.newDescription;
+
+    const updateFileContent = (filename, content, res) => {
+        fs.writeFile(`./files/${filename}`, content, (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error writing file');
+                return;
+            }
+            res.redirect('/');
+        });
+    };
+
+    if (previousFilename !== newFilename) {
+        fs.rename(`./files/${previousFilename}`, `./files/${newFilename}`, (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error renaming file');
+                return;
+            }
+            updateFileContent(newFilename, fileContent, res);
+        });
+    } else {
+        updateFileContent(previousFilename, fileContent, res);
+    }
+});
 
 app.listen(3000, () => {
     console.log(`server running on 3000`);
